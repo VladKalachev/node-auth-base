af68ac0de7780f524a288c12ffb8d7ce1ca20675
Corvax <vlvete1990@yandex.ru>
2021-05-28 21:58:07 +0300

    chore: added Navbar

--- client/src/routes.js
+++ client/src/routes.js
@@ -1,32 +1,35 @@
-import { Switch, Route, Redirect } from 'react-router-dom';
-import { LinksPage } from './pages/LinksPage';
-import { CreatePage } from './pages/CreatePage';
-import { DetailPage } from './pages/DetailPage';
-import { AuthPage } from './pages/AuthPage';
+
+import React from 'react'
+import {Switch, Route, Redirect} from 'react-router-dom'
+import {LinksPage} from './pages/LinksPage'
+import {CreatePage} from './pages/CreatePage'
+import {DetailPage} from './pages/DetailPage'
+import {AuthPage} from './pages/AuthPage'
 
 export const useRoutes = isAuthenticated => {
-  if(isAuthenticated) {
+  if (isAuthenticated) {
     return (
       <Switch>
-        <Route path='/linl' exact>
+        <Route path="/links" exact>
           <LinksPage />
         </Route>
-        <Route path='/create' exact>
+        <Route path="/create" exact>
           <CreatePage />
         </Route>
-        <Route path='/detail/:id'>
+        <Route path="/detail/:id">
           <DetailPage />
         </Route>
-        <Redirect to='/create' />
+        <Redirect to="/create" />
       </Switch>
     )
   }
+
   return (
-      <Switch>
-          <Route path='/' exact>
-              <AuthPage />
-          </Route>
-          <Redirect to='/' /> 
-      </Switch>
+    <Switch>
+      <Route path="/" exact>
+        <AuthPage />
+      </Route>
+      <Redirect to="/" />
+    </Switch>
   )
 }
